0ae413fc15b66084efee556ffc9e4f772c25335c
Niels Christian Konrad Nielsen <mail@nckn.dk>
2020-02-10 18:13:15 +0100

    Adding orbit controls

--- first-demo/src/scripts/app.js
+++ first-demo/src/scripts/app.js
@@ -1,252 +1,305 @@
-import 'styles/index.scss';
-import Cone from './elements/cone';
-import Torus from './elements/torus';
-import Cylinder from './elements/cylinder';
-import { radians, map, distance } from './helpers';
+import 'styles/index.scss'
+import Cone from './elements/cone'
+import Torus from './elements/torus'
+import Cylinder from './elements/cylinder'
+import { radians, map, distance } from './helpers'
+import Box from '../../../second-demo/src/scripts/elements/box'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 export default class App {
-  setup() {
-    this.gutter = { size: 4 };
-    this.meshes = [];
-    this.grid = { rows: 11, cols: 7 };
-    this.width = window.innerWidth;
-    this.height = window.innerHeight;
-    this.mouse3D = new THREE.Vector2();
+  setup () {
+    this.gutter = { size: 4 }
+    this.meshes = []
+    this.grid = { rows: 11, cols: 7 }
+    this.width = window.innerWidth
+    this.height = window.innerHeight
+    this.mouse3D = new THREE.Vector2()
     this.geometries = [
-      new Cone(),
-      new Torus(),
-      new Cylinder(),
-    ];
+      new Box()
+      // new Cone(),
+      // new Torus(),
+      // new Cylinder(),
+    ]
 
-    this.raycaster = new THREE.Raycaster();
+    this.raycaster = new THREE.Raycaster()
   }
 
-  createScene() {
-    this.scene = new THREE.Scene();
+  createScene () {
+    this.scene = new THREE.Scene()
 
-    this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
-    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true })
+    this.renderer.setSize(window.innerWidth, window.innerHeight)
+    this.renderer.setPixelRatio(window.devicePixelRatio)
 
+    this.renderer.shadowMap.enabled = true
+    this.renderer.shadowMap.type = THREE.PCFSoftShadowMap
 
-    this.renderer.shadowMap.enabled = true;
-    this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-
-    document.body.appendChild(this.renderer.domElement);
+    document.body.appendChild(this.renderer.domElement)
   }
 
-  createCamera() {
-    this.camera = new THREE.PerspectiveCamera(20, window.innerWidth / window.innerHeight, 1);
-    this.camera.position.set(0, 65, 0);
-    this.camera.rotation.x = -1.57;
+  createCamera () {
+    this.camera = new THREE.PerspectiveCamera(
+      20,
+      window.innerWidth / window.innerHeight,
+      1
+    )
+    this.camera.position.set(0, 65, 0)
+    this.camera.rotation.x = -1.57
 
-    this.scene.add(this.camera);
+    this.scene.add(this.camera)
   }
 
-  addAmbientLight() {
-    const light = new THREE.AmbientLight('#ffffff', 1);
-
-    this.scene.add(light);
+  createControls () {
+    // Camera and controls
+    this.orbitControls = new OrbitControls(
+      this.camera,
+      this.renderer.domElement
+    )
+    // this.orbitControls.enabled = true /* This disables touch devices */
+    // this.orbitControls.enabled = false /* Only with this as false touch devices work */
+    this.orbitControls.enableKeys = false
+    this.orbitControls.zoomSpeed = 0.5
+    // this.orbitControls.maxPolarAngle = Math.PI / 2 - Math.PI / 32 // Math.PI/2 is at floor
+    this.orbitControls.maxPolarAngle = Math.PI / 2 - 0.005 // Math.PI/2 is at floor
+    // No pan
+    this.orbitControls.enablePan = false
+    // this.instance = new THREE.PerspectiveCamera(40, this.sizes.viewport.width / this.sizes.viewport.height, 1, 180)
+    // Set max distance
+    this.orbitControls.maxDistance = 80
   }
 
-  addSpotLight() {
-    const ligh = new THREE.SpotLight('#7bccd7', 1, 1000);
+  addAmbientLight () {
+    const light = new THREE.AmbientLight('#ffffff', 1)
 
-    ligh.position.set(0, 27, 0);
-    ligh.castShadow = true;
-
-    this.scene.add(ligh);
+    this.scene.add(light)
   }
 
-  addRectLight() {
-    const light = new THREE.RectAreaLight('#341212', 1, 2000, 2000);
+  addSpotLight () {
+    const ligh = new THREE.SpotLight('#7bccd7', 1, 1000)
 
-    light.position.set(5, 50, 50);
-    light.lookAt(0, 0, 0);
+    ligh.position.set(0, 27, 0)
+    ligh.castShadow = true
 
-    this.scene.add(light);
+    this.scene.add(ligh)
   }
 
-  addPointLight(color, position) {
-    const light = new THREE.PointLight(color, 1, 1000, 1);
+  addRectLight () {
+    const light = new THREE.RectAreaLight('#341212', 1, 2000, 2000)
 
-    light.position.set(position.x, position.y, position.z);
+    light.position.set(5, 50, 50)
+    light.lookAt(0, 0, 0)
 
-    this.scene.add(light);
+    this.scene.add(light)
   }
 
-  addFloor() {
-    const geometry = new THREE.PlaneGeometry(100, 100);
-    const material = new THREE.ShadowMaterial({ opacity: .3 });
+  addPointLight (color, position) {
+    const light = new THREE.PointLight(color, 1, 1000, 1)
 
-    this.floor = new THREE.Mesh(geometry, material);
-    this.floor.position.y = 0;
-    this.floor.receiveShadow = true;
-    this.floor.rotateX(- Math.PI / 2);
+    light.position.set(position.x, position.y, position.z)
 
-    this.scene.add(this.floor);
+    this.scene.add(light)
   }
 
-  getRandomGeometry() {
-    return this.geometries[Math.floor(Math.random() * Math.floor(this.geometries.length))];
+  addFloor () {
+    const geometry = new THREE.PlaneGeometry(100, 100)
+    const material = new THREE.ShadowMaterial({ opacity: 0.3 })
+
+    this.floor = new THREE.Mesh(geometry, material)
+    this.floor.position.y = 0
+    this.floor.receiveShadow = true
+    this.floor.rotateX(-Math.PI / 2)
+
+    this.scene.add(this.floor)
   }
 
-  createGrid() {
-    this.groupMesh = new THREE.Object3D();
+  getRandomGeometry () {
+    return this.geometries[
+      Math.floor(Math.random() * Math.floor(this.geometries.length))
+    ]
+  }
+
+  createGrid () {
+    this.groupMesh = new THREE.Object3D()
 
     const material = new THREE.MeshPhysicalMaterial({
       color: '#3e2917',
-      metalness: .58,
+      metalness: 0.58,
       emissive: '#000000',
-      roughness: .05,
-    });
+      roughness: 0.05
+    })
 
     for (let row = 0; row < this.grid.rows; row++) {
-      this.meshes[row] = [];
+      this.meshes[row] = []
 
       for (let index = 0; index < 1; index++) {
-        const totalCol = this.getTotalRows(row);
+        const totalCol = this.getTotalRows(row)
 
         for (let col = 0; col < totalCol; col++) {
-          const geometry = this.getRandomGeometry();
-          const mesh = this.getMesh(geometry.geom, material);
+          const geometry = this.getRandomGeometry()
+          const mesh = this.getMesh(geometry.geom, material)
 
-          mesh.position.y = 0;
-          mesh.position.x = col + (col * this.gutter.size) + (totalCol === this.grid.cols ? 0 : 2.5);
-          mesh.position.z = row + (row * (index + .25));
+          mesh.position.y = 0
+          mesh.position.x =
+            col +
+            col * this.gutter.size +
+            (totalCol === this.grid.cols ? 0 : 2.5)
+          mesh.position.z = row + row * (index + 0.25)
 
-          mesh.rotation.x = geometry.rotationX;
-          mesh.rotation.y = geometry.rotationY;
-          mesh.rotation.z = geometry.rotationZ;
+          mesh.rotation.x = geometry.rotationX
+          mesh.rotation.y = geometry.rotationY
+          mesh.rotation.z = geometry.rotationZ
 
           mesh.initialRotation = {
             x: mesh.rotation.x,
             y: mesh.rotation.y,
-            z: mesh.rotation.z,
-          };
+            z: mesh.rotation.z
+          }
 
-          this.groupMesh.add(mesh);
+          this.groupMesh.add(mesh)
 
-          this.meshes[row][col] = mesh;
+          this.meshes[row][col] = mesh
         }
       }
     }
 
-    const centerX = -(this.grid.cols / 2) * this.gutter.size - 1;
-    const centerZ = -(this.grid.rows / 2) - .8;
+    const centerX = -(this.grid.cols / 2) * this.gutter.size - 1
+    const centerZ = -(this.grid.rows / 2) - 0.8
 
-    this.groupMesh.position.set(centerX, 0, centerZ);
+    this.groupMesh.position.set(centerX, 0, centerZ)
 
-    this.scene.add(this.groupMesh);
+    this.scene.add(this.groupMesh)
   }
 
-  getTotalRows(col) {
-    return (col % 2 === 0 ? this.grid.cols : this.grid.cols - 1);
+  getTotalRows (col) {
+    return col % 2 === 0 ? this.grid.cols : this.grid.cols - 1
   }
 
-  getMesh(geometry, material) {
-    const mesh = new THREE.Mesh(geometry, material);
+  getMesh (geometry, material) {
+    const mesh = new THREE.Mesh(geometry, material)
 
-    mesh.castShadow = true;
-    mesh.receiveShadow = true;
+    mesh.castShadow = true
+    mesh.receiveShadow = true
 
-    return mesh;
+    return mesh
   }
 
-  draw() {
-    this.raycaster.setFromCamera(this.mouse3D, this.camera);
+  draw () {
+    this.raycaster.setFromCamera(this.mouse3D, this.camera)
 
-    const intersects = this.raycaster.intersectObjects([this.floor]);
+    const intersects = this.raycaster.intersectObjects([this.floor])
 
     if (intersects.length) {
-      const { x, z } = intersects[0].point;
+      const { x, z } = intersects[0].point
 
       for (let row = 0; row < this.grid.rows; row++) {
         for (let index = 0; index < 1; index++) {
-          const totalCols = this.getTotalRows(row);
+          const totalCols = this.getTotalRows(row)
 
           for (let col = 0; col < totalCols; col++) {
-            const mesh = this.meshes[row][col];
+            const mesh = this.meshes[row][col]
 
-            const mouseDistance = distance(x, z,
+            const mouseDistance = distance(
+              x,
+              z,
               mesh.position.x + this.groupMesh.position.x,
-              mesh.position.z + this.groupMesh.position.z);
+              mesh.position.z + this.groupMesh.position.z
+            )
 
-            const y = map(mouseDistance, 7, 0, 0, 6);
-            TweenMax.to(mesh.position, .3, { y: y < 1 ? 1 : y });
+            const y = map(mouseDistance, 7, 0, 0, 6)
+            TweenMax.to(mesh.position, 0.3, { y: y < 1 ? 1 : y })
 
-            const scaleFactor = mesh.position.y / 1.2;
-            const scale = scaleFactor < 1 ? 1 : scaleFactor;
-            TweenMax.to(mesh.scale, .3, {
+            const scaleFactor = mesh.position.y / 1.2
+            const scale = scaleFactor < 1 ? 1 : scaleFactor
+            TweenMax.to(mesh.scale, 0.3, {
               ease: Expo.easeOut,
               x: scale,
               y: scale,
-              z: scale,
-            });
+              z: scale
+            })
 
-            TweenMax.to(mesh.rotation, .7, {
+            TweenMax.to(mesh.rotation, 0.7, {
               ease: Expo.easeOut,
-              x: map(mesh.position.y, -1, 1, radians(270), mesh.initialRotation.x),
-              z: map(mesh.position.y, -1, 1, radians(-90), mesh.initialRotation.z),
-              y: map(mesh.position.y, -1, 1, radians(45), mesh.initialRotation.y),
-            });
+              x: map(
+                mesh.position.y,
+                -1,
+                1,
+                radians(270),
+                mesh.initialRotation.x
+              ),
+              z: map(
+                mesh.position.y,
+                -1,
+                1,
+                radians(-90),
+                mesh.initialRotation.z
+              ),
+              y: map(
+                mesh.position.y,
+                -1,
+                1,
+                radians(45),
+                mesh.initialRotation.y
+              )
+            })
           }
         }
       }
     }
   }
 
-  init() {
-    this.setup();
+  init () {
+    this.setup()
 
-    this.createScene();
+    this.createScene()
 
-    this.createCamera();
+    this.createCamera()
+    
+    this.createControls()
 
-    this.createGrid();
+    this.createGrid()
 
-    this.addFloor();
+    this.addFloor()
 
-    this.addAmbientLight();
+    this.addAmbientLight()
 
-    this.addSpotLight();
+    this.addSpotLight()
 
-    this.addRectLight();
+    this.addRectLight()
 
-    this.addPointLight(0xfff000, { x: 0, y: 10, z: -100 });
+    this.addPointLight(0xfff000, { x: 0, y: 10, z: -100 })
 
-    this.addPointLight(0x79573e, { x: 100, y: 10, z: 0 });
+    this.addPointLight(0x79573e, { x: 100, y: 10, z: 0 })
 
-    this.addPointLight(0xc27439, { x: 20, y: 5, z: 20 });
+    this.addPointLight(0xc27439, { x: 20, y: 5, z: 20 })
 
-    this.animate();
+    this.animate()
 
-    window.addEventListener('resize', this.onResize.bind(this));
+    window.addEventListener('resize', this.onResize.bind(this))
 
-    window.addEventListener('mousemove', this.onMouseMove.bind(this), false);
+    window.addEventListener('mousemove', this.onMouseMove.bind(this), false)
 
-    this.onMouseMove({ clientX: 0, clientY: 0 });
+    this.onMouseMove({ clientX: 0, clientY: 0 })
   }
 
-  onMouseMove({ clientX, clientY }) {
-    this.mouse3D.x = (clientX / this.width) * 2 - 1;
-    this.mouse3D.y = -(clientY / this.height) * 2 + 1;
+  onMouseMove ({ clientX, clientY }) {
+    this.mouse3D.x = (clientX / this.width) * 2 - 1
+    this.mouse3D.y = -(clientY / this.height) * 2 + 1
   }
 
-  onResize() {
-    this.width = window.innerWidth;
-    this.height = window.innerHeight;
+  onResize () {
+    this.width = window.innerWidth
+    this.height = window.innerHeight
 
-    this.camera.aspect = this.width / this.height;
-    this.camera.updateProjectionMatrix();
-    this.renderer.setSize(this.width, this.height);
+    this.camera.aspect = this.width / this.height
+    this.camera.updateProjectionMatrix()
+    this.renderer.setSize(this.width, this.height)
   }
 
-  animate() {
-    this.draw();
+  animate () {
+    this.draw()
 
-    this.renderer.render(this.scene, this.camera);
+    this.renderer.render(this.scene, this.camera)
 
-    requestAnimationFrame(this.animate.bind(this));
+    requestAnimationFrame(this.animate.bind(this))
   }
 }
