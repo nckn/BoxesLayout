3af7009d1aaa086a89fa7abd1db9a770640c67f5
Niels Christian Konrad Nielsen <mail@nckn.dk>
2020-02-10 19:13:11 +0100

    Adding fog and background

--- first-demo/src/scripts/app.js
+++ first-demo/src/scripts/app.js
@@ -2,18 +2,17 @@ import 'styles/index.scss'
 // import Cone from './elements/cone'
 // import Torus from './elements/torus'
 // import Cylinder from './elements/cylinder'
-import { radians, map, distance } from './helpers'
+import { radians, map, distance, hexToRgbTreeJs } from './helpers';
 import Box from '../../../second-demo/src/scripts/elements/box'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 export default class App {
   setup () {
+    this.backgroundColor = '#191919'
 
-    this.backgroundColor = '#191919';
-
-    this.gutter = { size: 4 }
+    this.gutter = { size: 4 } // Org 4
     this.meshes = []
-    this.grid = { rows: 11, cols: 7 }
+    this.grid = { rows: 20, cols: 20 }
     this.width = window.innerWidth
     this.height = window.innerHeight
     this.mouse3D = new THREE.Vector2()
@@ -30,6 +29,9 @@ export default class App {
   createScene () {
     this.scene = new THREE.Scene()
 
+    this.scene.background = new THREE.Color(0x000000) // Black background
+    this.scene.fog = new THREE.Fog(0x000000, 20, 60)
+
     this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true })
     this.renderer.setSize(window.innerWidth, window.innerHeight)
     this.renderer.setPixelRatio(window.devicePixelRatio)
@@ -39,16 +41,16 @@ export default class App {
 
     document.body.appendChild(this.renderer.domElement)
   }
-  
+
   addControls () {
+    this.gui = new dat.GUI()
 
-    this.gui = new dat.GUI();
+    const gui = this.gui.addFolder('Background')
 
-    const gui = this.gui.addFolder('Background');
-
-    gui.addColor(this, 'backgroundColor').onChange((color) => {
-      document.body.style.backgroundColor = color;
-    });
+    gui.addColor(this, 'backgroundColor').onChange(color => {
+      document.body.style.backgroundColor = color
+      // this.scene.background = color
+    })
   }
 
   createCamera () {
@@ -79,22 +81,30 @@ export default class App {
     this.orbitControls.enablePan = false
     // this.instance = new THREE.PerspectiveCamera(40, this.sizes.viewport.width / this.sizes.viewport.height, 1, 180)
     // Set max distance
-    this.orbitControls.maxDistance = 80
+    this.orbitControls.maxDistance = 40
   }
 
   addAmbientLight () {
-    const light = new THREE.AmbientLight('#ffffff', 1)
+    const obj = { color: '#2900af' };
+    const light = new THREE.AmbientLight(obj.color, 1);
+    // const light = new THREE.AmbientLight('#ffffff', 1)
 
     this.scene.add(light)
+
+    const gui = this.gui.addFolder('Ambient Light');
+
+    gui.addColor(obj, 'color').onChange((color) => {
+      light.color = hexToRgbTreeJs(color);
+    });
   }
 
   addSpotLight () {
-    const ligh = new THREE.SpotLight('#7bccd7', 1, 1000)
+    const spLight = new THREE.SpotLight('#7bccd7', 1, 1000)
 
-    ligh.position.set(0, 27, 0)
-    ligh.castShadow = true
+    spLight.position.set(0, 27, 0)
+    spLight.castShadow = true
 
-    this.scene.add(ligh)
+    this.scene.add(spLight)
   }
 
   addRectLight () {
@@ -136,8 +146,9 @@ export default class App {
     this.groupMesh = new THREE.Object3D()
 
     const material = new THREE.MeshPhysicalMaterial({
-      color: '#3e2917',
-      metalness: 0.58,
+      // color: '#3e2917',
+      color: '#404040',
+      // metalness: 0.58,
       emissive: '#000000',
       roughness: 0.05
     })
@@ -152,16 +163,21 @@ export default class App {
           const geometry = this.getRandomGeometry()
           const mesh = this.getMesh(geometry.geom, material)
 
-          mesh.position.y = 0
-          mesh.position.x =
-            col +
-            col * this.gutter.size +
-            (totalCol === this.grid.cols ? 0 : 2.5)
-          mesh.position.z = row + row * (index + 0.25)
+          // mesh.position.y = 0
+          // mesh.position.x = col + col * this.gutter.size + (totalCol === this.grid.cols ? 0 : 2.5)
+          // mesh.position.z = row + row * (index + 0.25)
 
-          mesh.rotation.x = geometry.rotationX
-          mesh.rotation.y = geometry.rotationY
-          mesh.rotation.z = geometry.rotationZ
+          mesh.position.y = 0
+          mesh.position.x = col / 2 
+          mesh.position.z = row / 2
+
+          // mesh.rotation.x = geometry.rotationX
+          // mesh.rotation.y = geometry.rotationY
+          // mesh.rotation.z = geometry.rotationZ
+
+          mesh.rotation.x = 0
+          mesh.rotation.y = 0
+          mesh.rotation.z = 0
 
           mesh.initialRotation = {
             x: mesh.rotation.x,
@@ -176,10 +192,13 @@ export default class App {
       }
     }
 
-    const centerX = -(this.grid.cols / 2) * this.gutter.size - 1
-    const centerZ = -(this.grid.rows / 2) - 0.8
+    // const centerX = -(this.grid.cols / 2) * this.gutter.size - 1
+    // const centerZ = -(this.grid.rows / 2) - 0.8
+    
+    const centerX = -this.grid.cols / 4
+    const centerZ = -this.grid.rows / 4
 
-    this.groupMesh.position.set(centerX, 0, centerZ)
+    this.groupMesh.position.set(centerX, -0.75, centerZ)
 
     this.scene.add(this.groupMesh)
   }
@@ -219,42 +238,47 @@ export default class App {
               mesh.position.z + this.groupMesh.position.z
             )
 
-            const y = map(mouseDistance, 7, 0, 0, 6)
+            // const y = map(mouseDistance, 7, 0, 0, 6) // Org
+            const y = map(mouseDistance, 4, 0, 0, 4)
             TweenMax.to(mesh.position, 0.3, { y: y < 1 ? 1 : y })
 
-            const scaleFactor = mesh.position.y / 1.2
-            const scale = scaleFactor < 1 ? 1 : scaleFactor
-            TweenMax.to(mesh.scale, 0.3, {
-              ease: Expo.easeOut,
-              x: scale,
-              y: scale,
-              z: scale
-            })
+            // Scale happens here
+            // const scaleFactor = mesh.position.y / 1.2
+            // const scale = scaleFactor < 1 ? 1 : scaleFactor
+            // TweenMax.to(mesh.scale, 0.3, {
+            //   ease: Expo.easeOut,
+            //   x: scale,
+            //   y: scale,
+            //   z: scale
+            // })
+            // Scale happens here - end
 
-            TweenMax.to(mesh.rotation, 0.7, {
-              ease: Expo.easeOut,
-              x: map(
-                mesh.position.y,
-                -1,
-                1,
-                radians(270),
-                mesh.initialRotation.x
-              ),
-              z: map(
-                mesh.position.y,
-                -1,
-                1,
-                radians(-90),
-                mesh.initialRotation.z
-              ),
-              y: map(
-                mesh.position.y,
-                -1,
-                1,
-                radians(45),
-                mesh.initialRotation.y
-              )
-            })
+            // Rotation happens here
+            // TweenMax.to(mesh.rotation, 0.7, {
+            //   ease: Expo.easeOut,
+            //   x: map(
+            //     mesh.position.y,
+            //     -1,
+            //     1,
+            //     radians(270),
+            //     mesh.initialRotation.x
+            //   ),
+            //   z: map(
+            //     mesh.position.y,
+            //     -1,
+            //     1,
+            //     radians(-90),
+            //     mesh.initialRotation.z
+            //   ),
+            //   y: map(
+            //     mesh.position.y,
+            //     -1,
+            //     1,
+            //     radians(45),
+            //     mesh.initialRotation.y
+            //   )
+            // })
+            // Rotation happens here - end
           }
         }
       }
@@ -269,7 +293,7 @@ export default class App {
     this.addControls()
 
     this.createCamera()
-    
+
     this.createControls()
 
     this.createGrid()
